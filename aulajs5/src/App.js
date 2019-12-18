1764955942ae713b53a0fcb5d16e06b2cc515c23
vai na web <vainawebcpx@gmail.com>
2019-12-18 17:34:13 -0300

    brenno

---
+++ aulajs5/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import DB from './components/DB'
+
+function App() {
+  return (
+    <div className="App">
+      <DB/>
+    </div>
+  );
+}
+
+export default App;
---
+++ aulajs5/src/components/DB.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Dbz extends React.Component{
+    
+    render(){
+        return(
+            <header>
+                <div className="DBS-div">
+                <img className="DBS" src="https://logodownload.org/wp-content/uploads/2017/12/dragon-ball-super-logo.png"/>
+                <p>Dragon Ball Super (ドラゴンボール超スーパー, Doragon Bōru Sūpā?) é uma série de anime criada por Akira Toriyama e produzida pela Toei Animation, que começou a ser exibida em 5 de julho de 2015. É também uma série de mangá escrita/ilustrada por Toyotarō e supervisionada por Toriyama, serializada na revista de mangás shōnen V Jump da Shueisha. Dragon Ball Super é uma sequência para o mangá original Dragon Ball e a série de anime Dragon Ball Z, com o primeiro novo enredo em dezoito anos. É transmitida no Japão aos domingos às 9:00 da manhã na Fuji TV.</p>
+                <img src="https://www.konfest.com/wp-content/uploads/2019/05/Konfest-PNG-JPG-Image-Pic-Photo-Free-Download-Royalty-Unlimited-clip-art-sticker-Dragonballz-super-hero-goku-super-saiyan-blue-5.png" className="App-logo"/>
+                </div> 
+                <div className="GK-div">
+                <p>Goku (孫 悟空, Son Gokū, ou Son Goku), nascido Kakarotto (カカロット, Kakarotto) é o protagonista das franquias Dragon Ball. Ele é neto adotivo de Vovô Gohan, filho de Bardock e Gine, o irmão mais novo de Raditz, o marido de Chichi, pai de Gohan e Goten, avô de Pan e mais tarde tataravô de Goku Jr. Sua primeira aparição ocorreu em 1984, no primeiro capítulo do mangá Dragon Ball, publicado na revista Weekly Shōnen Jump. Goku é inspirado em Sun Wukong, personagem do romance Jornada ao Oeste. Porém, Toriyama mudou algumas de suas características para ser mais original. Goku é apresentado como um menino estranho com rabo de macaco e força sobre-humana.
+
+                Alegre, corajoso, e também um pouco ingênuo, Goku é um Saiyajin originalmente enviado à Terra quando criança com a missão de destruí-la. Porém, um acidente altera sua memória, fazendo-o crescer com um coração puro e depois se torna o maior defensor da Terra, e também o líder informal dos Guerreiros Z. Através da sua vida, ele treina duro e deseja ser o melhor guerreiro possível, e ao mesmo tempo usando sua força incrível para manter a paz.</p>
+                </div>
+            </header>
+        )
+    }
+} 
+export default Dbz;
